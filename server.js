5f33acd1bd4eb38485aacb22e2071cf9c64b6e59
Lourenco94 <nicolasrcl19@gmail.com>
2025-11-13 10:48:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/', function(req, res) {
 
 //Inicia o servidor na porta definida e exibe uma mensagem no console
 server.listen(porta, () => {
-    console.log(`JSON SERVER está rodando em http://localhost:${porta}`);
+    console.log(`JSON SERVER está rodando em 'pessoas'`);
 })
+
 
